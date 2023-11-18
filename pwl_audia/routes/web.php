c2c57f6452da491596ed9e04e9e35948b3a86c82
audiair <audiair04@gmail.com>
2023-11-18 09:05:24 +0700

    modul bab5 laravel brezee dan spatie laravel permission

--- pwl_audia/routes/web.php
+++ pwl_audia/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::view('/roles', 'role')->name('role')->middleware(['role:pustakawan']);
+});
+
+// Route::group(['middleware' => ['role:admin']], function(){
+//     //
+// });
+
+//multiple Roles
+// Route::group(['middleware' => ['role:admin|user']], function(){
+//     //
+// });
+
+require __DIR__.'/auth.php';
